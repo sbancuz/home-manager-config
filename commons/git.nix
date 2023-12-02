e45469b1e8d011a73580afb7397e5f0bd11202bc
sbancuz <sbancuz@protonmail.con>
2023-12-02 14:13:22 +0100

    a

--- commons/git.nix
+++ commons/git.nix
@@ -1,35 +1,35 @@
 { config, pkgs, lib, ... }:
 {
-  programs.git = {
-    package = pkgs.gitAndTools.gitFull;
-    enable = true;
-    userName = "sbancuz";
-    userEmail = "sbancuz@protonmail.com";
-    aliases = {
-      undo = "reset HEAD~1 --mixed";
-      amend = "commit -a --amend";
-      prv = "!gh pr view";
-      prc = "!gh pr create";
-      prs = "!gh pr status";
-      prm = "!gh pr merge -d";
-    };
-    extraConfig = {
-      color = {
-        ui = "auto";
-      };
-      diff = {
-        tool = "vimdiff";
-        mnemonicprefix = true;
-      };
-      merge = {
-        tool = "splice";
-      };
-      push = {
-        default = "simple";
-      };
-      pull = {
-        rebase = true;
-      };
-    };
-  };
+  # programs.git = {
+  #   package = pkgs.gitAndTools.gitFull;
+  #   enable = true;
+  #   userName = "sbancuz";
+  #   userEmail = "sbancuz@protonmail.com";
+  #   aliases = {
+  #     undo = "reset HEAD~1 --mixed";
+  #     amend = "commit -a --amend";
+  #     prv = "!gh pr view";
+  #     prc = "!gh pr create";
+  #     prs = "!gh pr status";
+  #     prm = "!gh pr merge -d";
+  #   };
+  #   extraConfig = {
+  #     color = {
+  #       ui = "auto";
+  #     };
+  #     diff = {
+  #       tool = "vimdiff";
+  #       mnemonicprefix = true;
+  #     };
+  #     merge = {
+  #       tool = "splice";
+  #     };
+  #     push = {
+  #       default = "simple";
+  #     };
+  #     pull = {
+  #       rebase = true;
+  #     };
+  #   };
+  # };
 }
